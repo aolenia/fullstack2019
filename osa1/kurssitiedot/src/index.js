0865067be8db0101a1911333d73a721f5c79e2bf
Antti Olenius <antti.olenius@cimcorp.com>
2019-12-09 15:42:24 +0200

    1.5

--- osa1/kurssitiedot/src/index.js
+++ osa1/kurssitiedot/src/index.js
@@ -1,9 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { exec } from 'child_process'
 
 const Header = (props) => {
-    return(<h1>{props.course}</h1>
+    return(<h1>{props.course.name}</h1>
     )
 }
 
@@ -76,7 +75,7 @@ const App = () => {
         </div>
       )
   }
-  */
+  
  const App = () => {
     const course = 'Half Stack application development'
     const parts = [
@@ -99,6 +98,34 @@ const App = () => {
         <Header course={course} />
         <Content parts={parts} />
         <Total amount={parts} />
+    </div>
+    )
+  }
+ */
+const App = () => {
+    const course = {
+      name: 'Half Stack application development',
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7
+        },
+        {
+          name: 'State of a component',
+          exercises: 14
+        }
+      ]
+    }
+  
+    return (
+        <div>
+        <Header course={course} />
+        <Content parts={course.parts} />
+        <Total amount={course.parts} />
     </div>
     )
   }
