310826b9977f5a19ff8e4b4c914d0fe06e51c556
Antti Olenius <antti.olenius@cimcorp.com>
2019-12-31 09:46:32 +0200

    right solutions to phonebook

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,97 +1,172 @@
 import React, { useState, useEffect } from 'react'
-import Persons from './components/Persons'
-import PersonForm from './components/PersonForm'
 import personService from './services/persons'
-import Notification from './components/Notification'
+
+const Notification = ({ notification }) => {
+  if (notification.message === null) {
+    return null
+  }
+
+  const style = {
+    color: notification.type === 'error' ? 'red' : 'green',
+    background: 'lightgrey',
+    fontSize: 20,
+    borderStyle: 'solid',
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 10,
+  }
+
+  return (
+    <div style={style}>
+      {notification.message}
+    </div>
+  )
+}
+
+const Filter = (props) => {
+  return (
+    <div>
+      rajaa näytettäviä
+      <input onChange={props.handleChange} value={props.value} />
+    </div>
+  )
+}
+
+const Persons = (props) => {
+  return (
+    props.persons.map(p =>
+      <div key={p.name}>
+        {p.name} {p.number} <button onClick={()=>props.deletePerson(p.id)}>poista</button>
+      </div>
+    )
+  )
+}
+
+const PersonForm = (props) => {
+  return (
+    <form onSubmit={props.handleSubmit}>
+      <div>
+        nimi: <input onChange={props.handleNameChange} value={props.newName} />
+      </div>
+      <div>
+        numero: <input onChange={props.handleNumberChange} value={props.newNumber} />
+      </div>
+      <div>
+        <button type="submit">lisää</button>
+      </div>
+    </form>
+  )
+}
 
 const App = () => {
   const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
-  const [notificationMessage, setNewNotification] = useState(null)
+  const [filter, setFilter] = useState('')
+  const [notification, setNotification] = useState({
+    message: null
+  })
 
   useEffect(() => {
-    console.log('effect')
-    personService
-      .getAll()
-      .then(startPersons => {
-        console.log('promise fulfilled')
-        setPersons(startPersons)
+    personService.getAll()
+      .then(data => {
+        setPersons(data)
       })
   }, [])
-  console.log('render', persons.length, 'notes')
 
-  const handleNameChange = (event) => {
-    setNewName(event.target.value)
-  }
-  const handleNumberChange = (event) => {
-    setNewNumber(event.target.value)
-  }
+  const handleNameChange = (event) => setNewName(event.target.value)
+  const handleNumberChange = (event) => setNewNumber(event.target.value)
+  const handleFilterChange = (event) => setFilter(event.target.value)
 
-  const setNotification = (message) => {
-    setNewNotification(
-      message
-    )
-    setTimeout(() => {
-      setNewNotification(null)
-    }, 2000)
+  const notify = (message, type='success') => {
+    setNotification({ message, type })
+    setTimeout(() => setNotification({ message: null }), 10000)
   }
-
-  const addPerson = (event) => {
+ 
+  const handleSubmit = (event) => {
     event.preventDefault()
-    const personObject = {
-      name: newName,
-      number: newNumber
-    }
 
-    if(persons.filter(person => person.name === newName).length > 0){
-      window.alert(`${newName} is already added to phonebook`)
-    }else{
+    const existingPerson = persons.find(p => p.name === newName)
+
+    if (existingPerson) {
+      const ok = window.confirm(`${newName} on jo luettelossa, korvataanko vanha numero uudella`)
+      
+
+      
+      if (ok) {
+        personService
+          .replace({
+            ...existingPerson,
+            number: newNumber
+          })
+          .then(replacedPerson => {
+            setPersons(persons.map(p => p.name === newName ? replacedPerson : p))
+            setNewName('')
+            setNewNumber('')
+            notify(`Henkilön ${newName} numero muutettu`)
+          })
+          /*
+          .catch(() => {
+            setPersons(persons.filter(p => p.name !== newName))
+            notify(`Henkilön ${newName} oli jo poistettu`, 'error')
+          })
+          */
+      }
+
+      return
+    } 
+     
+  personService
+      .create({
+        name: newName,
+        number: newNumber
+      })
+      .then(createdPerson => {
+        setPersons(persons.concat(createdPerson))
+        setNewName('')
+        setNewNumber('')
+        notify(`Lisättiin ${createdPerson.name}`)
+      })
+  }
+
+  const deletePerson = (id) => {
+    const person = persons.find(p => p.id === id)
+    const ok = window.confirm(`Poistetaanko ${person.name}`)
+    if (ok) {
       personService
-        .create(personObject)
-        .then(returnedPersons => {
-          console.log(returnedPersons)
-          setNotification(`Added ${newName} to phoneBook`)
-          setPersons(persons.concat(returnedPersons))
-          setNewName('')
-          setNewNumber('')
-        }) 
+        .remove(id)
+        .then(() => {
+          setPersons(persons.filter(p => p.id !== id))
+        })
+      notify(`Poistettiin ${person.name}`)
     }
   }
 
-  const deletePersonWithName = name => {
-    const person = persons.find(person => person.name === name)
-    const result = window.confirm(`Are you sure that you want to delete ${name}`)
-    console.log(result)
-    if(result){
-      personService
-      .deleteRequest(person.id)
-      .then(() => {
-        setPersons(persons.filter(p => p.id !== person.id))
-      })
-      setNotification(`Deleted ${name} from phonebook`)
-    }
-    }
+  const personsToShow = filter.length === 0
+    ? persons 
+    : persons.filter(p => p.name.toLowerCase().includes(filter.toLowerCase()) )
 
   return (
     <div>
-      <h2>Phonebook</h2>
-      <Notification message={notificationMessage}/>
-      <h3>  Add a new </h3>
+      <h2>Puhelinluettelo</h2>
+
+      <Notification notification={notification} />
+
+      <Filter handleChange={handleFilterChange} value={filter} />
+
+      <h3>lisää uusi</h3>
+
       <PersonForm 
+        handleNameChange={handleNameChange}
+        handleNumberChange={handleNumberChange}
+        handleSubmit={handleSubmit}
         newName={newName}
         newNumber={newNumber}
-        handleNameChange={() => handleNameChange}
-        handleNumberChange={() => handleNumberChange}
-        addPerson={() => addPerson}
       />
-      <h2>Numbers</h2>
-      <div>
-        <Persons 
-        persons={persons} 
-        deletePersonWithName={deletePersonWithName}
-        />
-      </div>
+
+      <h3>Numerot</h3>
+
+      <Persons persons={personsToShow} deletePerson={deletePerson} />
     </div>
   )
 
